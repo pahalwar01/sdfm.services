15ebe2017eb5650520f309f19f921282673f940f
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-02-28 10:48:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,3 +70,21 @@
     
 })(jQuery);
 
+
+ 
+    <script type="text/javascript">
+        function sendEmail() {
+            Email.send({
+                Host: "smtp.gmail.com",
+                Username: "sender@email_address.com",
+                Password: "Enter your password",
+                To: 'receiver@email_address.com',
+                From: "sender@email_address.com",
+                Subject: "Sending Email using javascript",
+                Body: "Well that was easy!!",
+            })
+                .then(function (message) {
+                    alert("mail sent successfully")
+                })
+        }
+    </script>
